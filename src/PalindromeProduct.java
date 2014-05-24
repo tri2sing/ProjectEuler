d2979cd1df5b1a2b94deb60f494623ebefaa15ec
Sameer Adhikari <tri2sing@yahoo.com>
2014-05-24 12:48:18 -0700

    Initial draft of file

--- src/PalindromeProduct.java
+++ src/PalindromeProduct.java
@@ -6,6 +6,14 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 
 public class PalindromeProduct {
 
+	// The largest product of any two three digit numbers is 999 * 999 = 998001.
+	// Hence we only need to generate 6 digit palindromes descending from 997799.
+	// We only need to go down to 100001 to at the most.
+	
+	public static void generatePalindromes () {
+		
+	}
+	
 	public static void main(String[] args) {
 
 	}
